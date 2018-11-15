4441b9567ae5375629cfc47d1f5ad5d7bb815609
ZYSzys <zyszys98@gmail.com>
2018-11-15 22:47:08 +0800

    simply todolist (#2)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import TodoItem from './components/Todo/TodoItem.js';
-import logo from './logo.svg';
+import TodoContainer from './components/TodoContainer';
 import './App.css';
 
 class App extends Component {
@@ -8,17 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <TodoItem />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <TodoContainer />
       </div>
     );
   }
---
+++ frontend/src/components/TodoContainer/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import TodoInput from './TodoInput.js'
+import TodoItem from './TodoItem.js';
+import './index.css';
+
+export default class TodoContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [
+                {
+                    'todo': '吃饭'
+                },
+                {
+                    'todo': '睡觉'
+                }
+            ]
+        }
+    };
+
+    addTodoItem = (todo) => {
+        this.setState(({list}) => ({
+            list: [{ todo }, ...list]
+        }));
+    };
+
+    deleteTodoItem = (needDelete) => {
+        this.setState(({list}) => ({
+            list: list.filter((todo, idx) => idx !== needDelete)
+        }));
+    };
+
+    render() {
+        return (
+            <div className="TodoContainer">
+                It's container
+                <TodoInput addTodoItem={this.addTodoItem}/>
+                {this.state.list.map((item, idx) =>
+                    <TodoItem
+                        key={idx}
+                        item={item.todo}
+                        deleteTodoItem={this.deleteTodoItem.bind(this, idx)}
+                    />)}
+            </div>
+        )
+    };
+}
