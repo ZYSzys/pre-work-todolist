69936c1ac671e602e1c17887ca9e152c289473f7
ZYSzys <17367077526@163.com>
2018-11-19 12:40:27 +0800

    deploy

---
+++ precache-manifest.d3964731cf2d7bc7551d648107be8a79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "63bac202574c1bf9b103",
+    "url": "/pre-work-todolist/static/css/main.58c8f821.chunk.css"
+  },
+  {
+    "revision": "63bac202574c1bf9b103",
+    "url": "/pre-work-todolist/static/js/main.63bac202.chunk.js"
+  },
+  {
+    "revision": "452cdb623f4a3d85c4a9",
+    "url": "/pre-work-todolist/static/css/1.2e83f74c.chunk.css"
+  },
+  {
+    "revision": "452cdb623f4a3d85c4a9",
+    "url": "/pre-work-todolist/static/js/1.452cdb62.chunk.js"
+  },
+  {
+    "revision": "d8462c45a718975f7fc2",
+    "url": "/pre-work-todolist/static/js/runtime~main.d8462c45.js"
+  },
+  {
+    "revision": "40e0f797929ce7b185ed654ae67b7f13",
+    "url": "/pre-work-todolist/static/media/undone.40e0f797.svg"
+  },
+  {
+    "revision": "6ef2b84790dbffc19d0e8bbfafbc861e",
+    "url": "/pre-work-todolist/static/media/done.6ef2b847.svg"
+  },
+  {
+    "revision": "b04fe2785807308246680e7681a7339e",
+    "url": "/pre-work-todolist/index.html"
+  }
+];
